e196bc2ad749fde5ce0da485df45af77fe4f519e
Manuel Vargas <manuelvargas1251@gmail.com>
2021-08-23 19:12:19 -0500

    🎨.0

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ function App() {
     <div className="App">
       <header>
         <p>
-          MNL SPACE
+          Manuel Vargas
         </p>
       </header>
       <div>
-        <Button link="https://www.youtube.com/" title="Music - Youtube" />
-        <Button link="https://www.instagram.com/" title="Music Merch - Instagram" />
+        <Button link="https://www.youtube.com/watch?v=ZtT553qLD2M" title="Music - Youtube" />
+        <Button link="https://www.instagram.com/shhmanuel/" title="Music Merch - Instagram" />
         <Button link="" title="Music Merch - Website" />
-        <Button link="" title="Hello:)" />
+        <Button link="https://manuelvargas.casa/" title="manuelvargas.casa" />
       </div>
     </div>
   );
